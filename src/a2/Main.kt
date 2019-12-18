eb50efef174af9ac7a4a94677883d9f7f9d4dee9
jomehr <janmehr93@gmx.de>
2019-12-18 16:29:03 +0100

    a2 fixe

--- src/a2/Main.kt
+++ src/a2/Main.kt
@@ -7,47 +7,46 @@ fun main () {
     //b)
     val product1 = Product("Apfel", 2.0, 3)
     val product2 = Product("Birnen", 40.0, 2)
-    val product3 = Product("Kirschen", 200.0, 5)
+    val product3 = Product("Kirschen", 20.0, 5)
     val product4 = Product("Salat", 3.0, 1)
-    val product5 = Product("Tomaten", 10.0, 4)
+    val product5 = Product("Tomaten", 100.0, 4)
 
     val list: List<Product> = List(product1, product2, product3, product4, product5)
 
     //1.
     val mapNames = map(list) {it.productName}
-    println(mapNames)
+    println("1) $mapNames")
 
     //2.
     val filterPrice = filter(list) {it.price > 10 }
     val mapNamesFilterPrice = map(filterPrice) {it.productName}
-    println(mapNamesFilterPrice)
+    println("2) $mapNamesFilterPrice")
 
     //3.
     val mapPricier = map(list) {it.price * 1.19}
-    println(mapPricier)
+    println("3) $mapPricier")
 
     //4.
     val filterRating = filter(list) {it.rating >= 3}
     val mapNamedFilterRating = map(filterRating) {it.productName}
-    println(mapNamedFilterRating)
+    println("4) $mapNamedFilterRating")
 
     //5.
     val replaceName = replaceIf(list, {Product("Angebot:"+it.productName, it.price, it.rating)}, {it.productName.startsWith("A") || it.productName.startsWith("B")})
-    println(replaceName)
+    println("5) $replaceName")
 
     //6.
     val anyPrice = any(list) {it.price > 100}
     val anyIt = anyIteration(list) {it.price > 100}
-    println(anyPrice)
-    println("test"+anyIt)
+    println("6) $anyPrice")
 
     //7.
     val foldSum = fold(list, 0.0, {total, next -> total + next.price})
-    println(foldSum)
+    println("7) $foldSum")
 
     //8
-    val foldMax = fold(list, 0.0, {max,next -> if(max < next.price) max == next.price})
-
+    val foldMax = fold(list, 0.0, {max, next -> maxOf(max, next.price)})
+    println("8) $foldMax")
 
     //c)
     val kaeseFactory = productFactory("Käse" , 5)
@@ -90,7 +89,7 @@ fun <T> anyIteration (list: List<T>, p: (T) -> Boolean): Boolean {
     return false
 }
 
-fun <T> anyFold (list: List<T>, p: (T) -> Boolean): Boolean = fold(list, false, )
+//fun <T> anyFold (list: List<T>, p: (T) -> Boolean): Boolean = fold(list, false, )
 
 
 tailrec fun <T,R> fold( list : List <T> , accumulated : R , f : (R,T)->R ) : R = when(list) {
